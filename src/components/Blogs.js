994262f8cb061538ec3681761a5cfc67f4cf9328
7ommyShelby <aryaalsaher@gmail.com>
2024-03-03 13:13:25 +0530

    animation

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,9 +1,18 @@
 import './blog.css'
 import img from '../img/smimg.avif'
 import Rev from './Rev.js'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import { useEffect } from "react";
+
+const Blog = () => {
 
 
-const blog = () => {
+    useEffect(() => {
+        AOS.init();
+    }, [])
+
 
     const arr = [{
         review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet, molestiae aliquam incidunt suscipit recusandae labore ratione doloremque, architecto et illo minus quo tenetur ducimus, voluptatibus repellendus fuga aperiam vel ab! Ipsam corrupti blanditiis dolorum! Officia assumenda rem nam, eveniet enim ad inventore laudantium est illum voluptatem quis.",
@@ -35,7 +44,7 @@ const blog = () => {
     return (
         <>
             <section className="sec3 flex">
-                <div className="gr flex gap-8">
+                <div className="gr flex gap-8 " data-aos="fade-right">
 
                     {
                         arr.map((elem, idx) => {
@@ -53,4 +62,4 @@ const blog = () => {
         </>
     )
 }
-export default blog;
+export default Blog;
